5dcc9589fc0f88a5fe78c15a23e5d73953ea487f
wd30130 <wd30130@aliyun.com>
2024-07-12 22:58:19 +0800

    fix(cli): fix script files

--- scripts/init.sh
+++ scripts/init.sh
@@ -3,5 +3,5 @@
 # Initializing build environment
 
 
-rustup install nightly-2021-03-01
-rustup target add wasm32-unknown-unknown --toolchain nightly-2020-10-06-x86_64-unknown-linux-gnu
+rustup install stable
+rustup target add wasm32-unknown-unknown
